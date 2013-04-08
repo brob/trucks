b1460d685030a622e3ad80e3efd46855e6dace46
Bryan Robinson <bryanlrobinson@gmail.com>
2013-04-08 12:38:22 -0500

    template dir tweak for server

--- trucks/trucks/settings.py
+++ trucks/trucks/settings.py
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/bryanlrobinson/webapps/custom/trucks/templates',
+	'/home/bryanlrobinson/webapps/custom/trucks/trucks/templates',
 	'/Users/bryanrobinson/documents/personal/trucks/trucks/templates',
 
 )
