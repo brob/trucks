e9859b2e8ba7700c0b87e421dd8d2d984a03d82c
Bryan Robinson <bryanlrobinson@gmail.com>
2013-04-08 18:10:10 -0500

    Settings media change 2

--- trucks/trucks/settings.py
+++ trucks/trucks/settings.py
@@ -109,6 +109,7 @@ if os.getenv(RUN_ENV, '') == 'production':
 	    # Don't forget to use absolute paths, not relative paths.
 		'/'
 	)
+	STATIC_URL = "http://media.bryanlrobinson.com"
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'x(4ph8)un3au+9x-v+&d7hnrv9(#i05_07_387m!ahn1vy_m%m'
