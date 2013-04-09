2d11b20b418dc0fd0425da89124f61ea15ff6be4
Bryan Robinson <bryanlrobinson@gmail.com>
2013-04-09 10:10:11 -0500

    changing root

--- trucks/trucks/settings.py
+++ trucks/trucks/settings.py
@@ -81,11 +81,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/bryanlrobinson/webapps/static_port/media/django-media/'
+STATIC_ROOT = '/home/bryanlrobinson/webapps/static_port/static-trucks/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static-trucks/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
