e6569c16cbd243c465b5594a400a95886489e268
Bryan Robinson <bryanlrobinson@gmail.com>
2013-04-09 09:39:04 -0500

    Revert "static url"
    This reverts commit fe9f10a5f94ec9b40aaa7f65848c6eab41d8d5a5.

--- trucks/trucks/settings.py
+++ trucks/trucks/settings.py
@@ -86,6 +86,9 @@ STATIC_ROOT = '/home/bryanlrobinson/webapps/static_port/'
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
+#STATIC_URL = 'http://media.bryanlrobinson.com/media/django-media/static/'
+if os.getenv(RUN_ENV, '') == 'production':
+	STATIC_URL = '/static-trucks/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
