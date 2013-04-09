7c616b42630f527fa0c6416b7315bdaac066a985
Bryan Robinson <bryanlrobinson@gmail.com>
2013-04-08 20:05:19 -0500

    wildcard allowed hosts

--- trucks/trucks/settings.py
+++ trucks/trucks/settings.py
@@ -41,8 +41,7 @@ if os.getenv(RUN_ENV, '') == 'production':
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
-	'beta.bryanlrobinson.com',
-	'media.bryanlrobinson.com',
+	'.bryanlrobinson.com',
 ]
 
 # Local time zone for this installation. Choices can be found here:
