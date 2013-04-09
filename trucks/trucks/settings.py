908bf11a5debc511a934b7a3536cce8e1d35a3ab
Bryan Robinson <bryanlrobinson@gmail.com>
2013-04-09 10:01:44 -0500

    Static

--- trucks/trucks/settings.py
+++ trucks/trucks/settings.py
@@ -81,11 +81,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/bryanlrobinson/webapps/static_port/'
+STATIC_ROOT = '/home/bryanlrobinson/webapps/static_port/media/django-media/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static-trucks/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
