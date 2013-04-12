f25159e6d526af3fd769627a76ed456ee6bddb43
Bryan Robinson <bryanlrobinson@gmail.com>
2013-04-12 15:03:47 -0500

    static files prod stuff

--- trucks/trucks/settings.py
+++ trucks/trucks/settings.py
@@ -91,17 +91,20 @@ STATIC_ROOT = '/home/bryanlrobinson/webapps/static_port/static-trucks/'
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.	
-	'/home/bryanlrobinson/webapps/static_port/media/django-media/static',
-	'/home/bryanlrobinson/webapps/static_port/static-trucks/Truck-static-files',
-	#'/home/bryanlrobinson/webapps/static_port/media/django-media/static-trucks/Truck-static-files',
 	'/Users/bryanrobinson/documents/personal/static/trucks',
 )
+if os.getenv(RUN_ENV, '') == 'production':
+
+	# Additional locations of static files
+	STATICFILES_DIRS = (
+	    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+	    # Always use forward slashes, even on Windows.
+	    # Don't forget to use absolute paths, not relative paths.	
+		'/home/bryanlrobinson/webapps/static_port/media/django-media/static',
+		'/home/bryanlrobinson/webapps/static_port/static-trucks/Truck-static-files',
+	
+	)
 
 # List of finder classes that know how to find static files in
 # various locations.
