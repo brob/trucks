7ce9a4a07a7b5bb04bb7e2e6e18cde4be6681b52
Bryan Robinson <bryanlrobinson@gmail.com>
2013-04-20 10:30:36 -0500

    Switched template dirs to go to new repo set up

--- trucks/trucks/settings.py
+++ trucks/trucks/settings.py
@@ -131,7 +131,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 	'/home/bryanlrobinson/webapps/custom/trucks/templates',
-	'/Users/bryanrobinson/documents/personal/trucks/trucks/templates',
+	'/Users/bryanrobinson/documents/personal/templates/Truck Templates',
 
 )
 
